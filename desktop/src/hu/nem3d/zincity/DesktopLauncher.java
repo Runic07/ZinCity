26ec9c473a8597b04e3fd6a72e2f9e921885f164
Jakab Márton Zoltán <x5mc0i@inf.elte.hu>
2023-03-16 14:52:30 +0100

    initial commit

---
+++ desktop/src/hu/nem3d/zincity/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package hu.nem3d.zincity;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import hu.nem3d.zincity.ZinCity;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ZinCity");
+		new Lwjgl3Application(new ZinCity(), config);
+	}
+}
