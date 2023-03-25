27b3cec5744cbceb7c80ff7386ee1f1613a97cb5
Marci Jakab <jakabxmarci@gmail.com>
2023-03-25 21:08:45 +0100

    initial draft of map with placeholder tiles

--- core/src/hu/nem3d/zincity/ZinCity.java
+++ core/src/hu/nem3d/zincity/ZinCity.java
@@ -4,28 +4,26 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class ZinCity extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+
+
+public class ZinCity extends Game {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new GameScreen());
+		System.out.println("Present Project Directory : "+ System.getProperty("user.dir"));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
