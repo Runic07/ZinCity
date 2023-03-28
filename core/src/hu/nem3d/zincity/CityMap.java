52e6243e2f50701501f020960db0c7e253563464
Wafflesmackk <81386123+Wafflesmackk@users.noreply.github.com>
2023-03-28 18:45:23 +0200

    Catching up to master for merge

---
+++ core/src/hu/nem3d/zincity/CityMap.java
@@ -0,0 +1,53 @@
+package hu.nem3d.zincity;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+
+
+public class CityMap {
+    TiledMap map;
+    TiledMapTileLayer baseLayer; //contains Cell objects that point to TiledMapTile objects
+    TiledMapTileLayer buildingLayer; //more layers can be added on demand
+    TiledMapTileSet tileSet; //contains TiledMapTile objects.
+    Texture texture;
+    public CityMap() {
+        texture = new Texture(System.getProperty("user.dir") + "\\assets\\texture.png");
+
+        //create the tileset
+        tileSet = new TiledMapTileSet();
+        int i = 0, j = 0;
+        for (TextureTiles item : TextureTiles.values()) {
+            if (i > 9){ //since there are 10 columns in the texture file.
+                i = 0;
+                j++;
+            }
+            tileSet.putTile(item.ordinal(), new StaticTiledMapTile(new TextureRegion(texture, i*24, j*24, 24,24)));
+            i++;
+
+        }
+
+        //create the layer
+        baseLayer = new TiledMapTileLayer(30,20,24,24);
+        buildingLayer = new TiledMapTileLayer(30,20,24,24);
+        //fill the layer with tiles. Everything is grass now.
+        for ( i = 0; i < 30; i++) {
+            for ( j = 0; j < 20; j++) {
+                CityCell cell = new CityCell();
+                cell.setTile(tileSet.getTile(0));
+                baseLayer.setCell(i, j, cell);
+            }
+        }
+
+        map = new TiledMap();
+        map.getLayers().add(baseLayer);
+        map.getLayers().add(buildingLayer);
+    }
+    public TiledMap getMap(){
+        return map;
+    }
+}
--- core/src/hu/nem3d/zincity/GameScreen.java
+++ core/src/hu/nem3d/zincity/GameScreen.java
@@ -2,61 +2,25 @@ package hu.nem3d.zincity;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.MapLayer;
+
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.tiles.*;
+
 
 
 
 
 public class GameScreen implements Screen { //draft
     ZinCity zinCity;
+    CityMap cityMap;
     OrthographicCamera camera;
-    TiledMap map;
-    TiledMapTileLayer baseLayer; //contains Cell objects that point to TiledMapTile objects
-    TiledMapTileLayer buildingLayer; //more layers can be added on demand
-    TiledMapTileSet tileSet; //contains TiledMapTile objects.
-    Texture texture;
-    StaticTiledMapTile tile_grass;
-    StaticTiledMapTile tile_water;
     TiledMapRenderer mapRenderer;
     public GameScreen(){
-        //create the tiles
-        texture = new Texture(System.getProperty("user.dir") + "\\assets\\texture.png");
-        tile_grass = new StaticTiledMapTile(new TextureRegion(texture,0,0,24,24));
-        tile_water = new StaticTiledMapTile(new TextureRegion(texture,24,0,24,24)); //todo check if coordinates are measured the standard way.
-        //create the tileset
-        tileSet = new TiledMapTileSet();
-        tileSet.putTile(0, tile_grass);
-        tileSet.putTile(1, tile_water);
-        //create the layer
-        baseLayer = new TiledMapTileLayer(30,20,24,24);        //fill the layer with tiles
-        for (int i = 0; i < 30; i++) {
-            for (int j = 0; j < 10; j++) {
-                TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell(); //NOT the type of cell a table has!
-                cell.setTile(tileSet.getTile(0));
 
-                baseLayer.setCell(i,j,cell);
-
-            }
-            for (int j = 10; j < 20; j++) {
-                TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell(); //NOT the type of cell a table has!
-                cell.setTile(tileSet.getTile(1));
-
-                baseLayer.setCell(i,j,cell);
-            }
-        }
-
-        //add the layer to the map
-        map = new TiledMap();
-        map.getLayers().add(baseLayer);
-
+        cityMap = new CityMap();
         //render map
         float unitScale = 1 / 24f;
-        mapRenderer = new OrthogonalTiledMapRenderer(map, unitScale);
+        mapRenderer = new OrthogonalTiledMapRenderer(cityMap.getMap(), unitScale);
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 30, 20);
         mapRenderer.setView(camera);
