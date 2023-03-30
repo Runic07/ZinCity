62ffe289281c3b96bd16784290fc68c35055ee2c
Daka Bence Vince <i5rqfi@inf.elte.hu>
2023-03-30 23:30:25 +0200

    Bugfix mainly
    Fixed issue 25, and finished the main structure behind menuBar

--- core/src/hu/nem3d/zincity/GameScreen.java
+++ core/src/hu/nem3d/zincity/GameScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 
@@ -47,7 +48,7 @@ public class GameScreen implements Screen { //draft
         UICamera.setToOrtho(false, (float) (screenWidth * 1.5), (float) (screenHeight*1.5));
 
         batch = new SpriteBatch();
-        //viewport = new FitViewport(width, height, UICamera);
+        //viewport = new FitViewport(screenWidth, screenHeight, UICamera);
         //Creating a virtual viewport with 1.5 times the size of the window as to scale the UI properly
         viewport = new FillViewport((float) (screenWidth *1.5), (float) (screenHeight * 1.5), UICamera);
         viewport.apply();
@@ -60,10 +61,10 @@ public class GameScreen implements Screen { //draft
     public void show() {
         Gdx.input.setInputProcessor(stage);
         Table table = new Table();
-        table = menuBar.setTable(uiId);
+        table = menuBar.setTable(uiId, screenWidth, screenHeight);
         //Setting the bounds og the UI to start at 0 at 95% of the virtual ScreenHeight and with 100% of the virtual screen width
         //and to be 15% of the normal screen height (so the size is 100% width and 15% of height at the top of screen
-        table.setBounds(0, (float) (screenHeight * 0.95 *1.5), (float) ((float) screenWidth *1 * 1.5), (float) (screenHeight * 0.15));
+        table.setBounds(0, (float) (screenHeight * 0.90 *1.5), (float) ((float) screenWidth *1 * 1.5), (float) (screenHeight * 0.15));
         stage.addActor(table);
 
     }
@@ -77,6 +78,15 @@ public class GameScreen implements Screen { //draft
 
     @Override
     public void resize(int width, int height) {
+        screenWidth = width;
+        screenHeight = height;
+        UICamera = new OrthographicCamera();
+        UICamera.setToOrtho(false, (float) (screenWidth * 1.5), (float) (screenHeight*1.5));
+        viewport = new FillViewport((float) (screenWidth *1.5), (float) (screenHeight * 1.5), UICamera);
+        viewport.apply();
+        stage = new Stage(viewport, batch);
+        menuBar = new MenuBar(stage);
+        this.show();
     }
 
     @Override
--- core/src/hu/nem3d/zincity/MenuBar.java
+++ core/src/hu/nem3d/zincity/MenuBar.java
@@ -16,7 +16,7 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import javax.swing.*;
 import javax.swing.plaf.synth.SynthTableHeaderUI;
 
-public class MenuBar{
+public class MenuBar {
 
 
     private TextureAtlas atlas;
@@ -26,13 +26,13 @@ public class MenuBar{
 
     private Stage stage;
 
-    public MenuBar(Stage stage_){
+    public MenuBar(Stage stage_) {
         atlas = new TextureAtlas(Gdx.files.internal("PlaceHolderMenu\\uiskin.atlas"));
         skin = new Skin(Gdx.files.internal("PlaceHolderMenu\\uiskin.json"), atlas);
         stage = stage_;
     }
 
-    public Table setTable(int id) {
+    public Table setTable(int id, final float width, final float height) {
         //switch case for id
 
         //Create Table
@@ -43,76 +43,74 @@ public class MenuBar{
         //Set alignment of contents in the table.
         currTable.top();
 
-        float height = (float) (Gdx.graphics.getHeight() * 0.15);
-        float width = (float) Gdx.graphics.getWidth();
-
-        //Create buttons
-        TextButton buildButton = new TextButton("Build", skin);
-        buildButton.setSize(width/8, (float)((height * 0.15) / 2));
-
-        TextButton statButton = new TextButton("Stats", skin);
-        statButton.setSize(width/8, (float)((height * 0.15) / 2));
-
         TextButton exitButton = new TextButton("Exit", skin);
-        exitButton.setSize(width/8, (float)((height * 0.15) / 2));
+        exitButton.setSize(width / 9, (float) ((height * 0.15) / 2));
+                //Create buttons
+                TextButton buildButton = new TextButton("Build", skin);
+                buildButton.setSize(width / 9, (float) ((height * 0.15) / 2));
 
-        Label happiness = new Label("50% ", skin);
-        happiness.setSize(width/8, (float)((height * 0.15) / 2));
+                TextButton statButton = new TextButton("Stats", skin);
+                statButton.setSize(width / 9, (float) ((height * 0.15) / 2));
 
-        Label date = new Label("2023.03.30 ", skin);
-        date.setSize(width/8, (float)((height * 0.15) / 2));
 
-        Label money = new Label("500$ ", skin);
-        money.setSize(width/8, (float)((height * 0.15) / 2));
+                Label happiness = new Label("50% ", skin);
+                happiness.setSize(width / 9, (float) ((height * 0.15) / 2));
 
-        //Add listeners to buttons
-        buildButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-            }
-        });
-        statButton.addListener((new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-            }
-        }));
-        exitButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Dialog dialog = new Dialog("Warning", skin, "dialog") {
-                    public void result(Object obj) {
-                        System.out.println("result " + obj);
-                        boolean quitMenu = (Boolean) obj;
-                        if (quitMenu) {
-                            Gdx.app.exit();
-                        }
+                Label date = new Label("2023.03.30 ", skin);
+                date.setSize(width / 9, (float) ((height * 0.15) / 2));
+
+                Label money = new Label("500$ ", skin);
+                money.setSize(width / 9, (float) ((height * 0.15) / 2));
+
+                //Add listeners to buttons
+                buildButton.addListener(new ClickListener() {
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        
                     }
-                };
-                dialog.text("Are you sure you want to quit?");
-                dialog.button("Yes", true);
-                dialog.button("No", false);
-                dialog.getBackground().setMinWidth(200);
-                dialog.getBackground().setMinHeight(200);
-                dialog.show(stage);
+                });
+                statButton.addListener((new ClickListener() {
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        MenuBar tmp = new MenuBar(stage);
+                        currTable = tmp.setTable(1,width,height);
+                    }
+                }));
+                exitButton.addListener(new ClickListener() {
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        Dialog dialog = new Dialog("Warning", skin, "dialog") {
+                            public void result(Object obj) {
+                                System.out.println("result " + obj);
+                                boolean quitMenu = (Boolean) obj;
+                                if (quitMenu) {
+                                    Gdx.app.exit();
+                                }
+                            }
+                        };
+                        dialog.text("Are you sure you want to quit?");
+                        dialog.button("Yes", true);
+                        dialog.button("No", false);
+                        dialog.getBackground().setMinWidth(200);
+                        dialog.getBackground().setMinHeight(200);
+                        dialog.show(stage);
 
-            }
-        });
-        //currTable.setBounds( 0, (float) (height * 0.8), width, (float) (height * 0.15));
-        //currTable.setSize((float) Gdx.graphics.getWidth(), (float) (Gdx.graphics.getHeight() * 0.15));
-        //Add rows to table
-        //currTable.add(BuildButton);
-        currTable.add(buildButton).spaceRight(10).expand().bottom().fillX();
-        //currTable.add(statButton);
-        currTable.add(statButton).spaceRight(10).expand().bottom().fillX();
-        //currTable.add(exitButton);
-        currTable.add(exitButton).spaceRight(10).expand().bottom().fillX();
-        //currTable.add(happiness);
-        currTable.add(happiness).spaceRight(10).expand().bottom().fillX();
-        //currTable.add(date);
-        currTable.add(date).spaceRight(10).expand().bottom().fillX();
-        currTable.add(money).expand().bottom().fillX();
-
-
+                    }
+                });
+                //currTable.setBounds( 0, (float) (height * 0.8), width, (float) (height * 0.15));
+                //currTable.setSize((float) Gdx.graphics.getWidth(), (float) (Gdx.graphics.getHeight() * 0.15));
+                //Add rows to table
+                //currTable.add(BuildButton);
+                currTable.add(buildButton).spaceRight(10).expand().bottom().fill();
+                //currTable.add(statButton);
+                currTable.add(statButton).spaceRight(10).expand().bottom().fill();
+                //currTable.add(exitButton);
+                currTable.add(exitButton).spaceRight(10).expand().bottom().fill();
+                //currTable.add(happiness);
+                currTable.add(happiness).spaceRight(10).expand().bottom().fill();
+                //currTable.add(date);
+                currTable.add(date).spaceRight(10).expand().bottom().fill();
+                currTable.add(money).expand().bottom().fill();
         return currTable;
 
     }
