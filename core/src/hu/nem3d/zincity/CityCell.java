72bf91d9b19a0370d60053639559cef3cf1fdba0
Runic07 <100705469+Runic07@users.noreply.github.com>
2023-04-01 17:29:43 +0200

    Initial commit

--- core/src/hu/nem3d/zincity/CityCell.java
+++ core/src/hu/nem3d/zincity/CityCell.java
@@ -6,7 +6,49 @@ public class CityCell extends TiledMapTileLayer.Cell {
     //CLASS FOR EACH TILE ON THE MAP
     //should contain logic data.
     //equvivalent with "tile" class in UML diagram. Renamed to avoid confusion.
+
+    //For saving coordinates, may be removed later
+    private int x, y;
+
+    //It will may need a rework, but now for efficiency purposes, those are 2 booleans
+    //Obviously, each tile starts with no wire, electricity
+    private boolean isElectrified = false;
+    private boolean isWired = false;
+
+    //placeholder constructor, may be removed later
     public CityCell() {
         super();
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public CityCell(int x, int y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    //Basic setters, getters
+    public int getX() {return x;}
+    public void setX(int x) {this.x = x;}
+
+    public int getY() {return y;}
+    public void setY(int y) {this.y = y;}
+
+    public boolean isWired() {return isWired;}
+    public void setWired(boolean wired) {isWired = wired;}
+
+    public boolean isElectrified() {return isElectrified;}
+    public void setElectrified(boolean elect) {
+        if (elect) {
+            if (isWired) {  //Electricity requires wires to flow through
+                isElectrified = true;
+            } else {
+                throw new IllegalArgumentException("Can't have electricity without wires!");
+            }
+        } else {
+            isElectrified = false;
+        }
     }
 }
+
