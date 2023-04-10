dc267e2a1d2ff890f6de3b2185f2e13115d1c258
Bertóti Bálint <bcv6vb@inf.elte.hu>
2023-04-10 17:46:26 +0200

    Adding forest rework suggestion

--- core/src/hu/nem3d/zincity/Cell/ForestCell.java
+++ core/src/hu/nem3d/zincity/Cell/ForestCell.java
@@ -15,4 +15,22 @@ public class ForestCell extends CityCell{
     public int getAge() {return age;}
     public void setAge(int age) {this.age = age;}
     public void increaseAge(){age = (age == 10) ? 10 : age+1;}
+
+
+    /*
+    private int plantingYear;
+
+    public ForestCell() {
+        this.plantingYear = 2023;
+    }
+
+    public ForestCell(int x, int y, int plantingYear) {
+        super(x, y);
+        this.plantingYear = plantingYear;
+    }
+
+    public int getPlantingYear() {return plantingYear;}
+    public void setPlantingYear(int plantingYear) {this.plantingYear = plantingYear;}
+    public int getAge(int yearNow){return (yearNow - plantingYear);}
+    */
 }
