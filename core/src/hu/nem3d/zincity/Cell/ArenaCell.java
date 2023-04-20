74579ab6f878e9a0c9c528b4a02d71eee57cc315
Daka Bence Vince <i5rqfi@inf.elte.hu>
2023-04-20 15:19:59 +0200

    Removed junk

--- core/src/hu/nem3d/zincity/Cell/ArenaCell.java
+++ core/src/hu/nem3d/zincity/Cell/ArenaCell.java
@@ -1,12 +1,16 @@
 package hu.nem3d.zincity.Cell;
 
-public class ArenaCell extends BuildingCell{   //It's a dummy class for building implementation
-
-    public ArenaCell(int range, int maintenanceFee){
+public class ArenaCell extends BuildingCell {
+    public ArenaCell(int range, int maintenanceFee) {
         super(range, maintenanceFee, false);
         this.name = "Arena";
     }
-    @Override
-    public void doEffect(){
+
+    public ArenaCell(int x, int y, int range, int maintenanceFee) {
+        super(x, y, range, maintenanceFee, false);
+    }
+
+    public ArenaCell(int x, int y, int range, int maintenanceFee, BuildingPart part) {
+        super(x, y, range, maintenanceFee, part);
     }
 }
--- core/src/hu/nem3d/zincity/Cell/GeneratorCell.java
+++ core/src/hu/nem3d/zincity/Cell/GeneratorCell.java
@@ -1,11 +1,16 @@
 package hu.nem3d.zincity.Cell;
 
-public class GeneratorCell extends BuildingCell{
-    public GeneratorCell(int range, int maintenanceFee){
+public class GeneratorCell extends BuildingCell {
+    public GeneratorCell(int range, int maintenanceFee) {
         super(range, maintenanceFee, false);
         this.name = "Generator";
     }
-    @Override
-    public void doEffect(){
+
+    public GeneratorCell(int x, int y, int range, int maintenanceFee) {
+        super(x, y, range, maintenanceFee, false);
+    }
+
+    public GeneratorCell(int x, int y, int range, int maintenanceFee, BuildingPart part) {
+        super(x, y, range, maintenanceFee, part);
     }
 }
