38c42562a748f9dfb7ef205f9fc2fc8ddbc2ecc1
Bertóti Bálint <bcv6vb@inf.elte.hu>
2023-04-23 18:34:55 +0200

    Adding documentation

--- core/src/hu/nem3d/zincity/Misc/DistanceCalculator.java
+++ core/src/hu/nem3d/zincity/Misc/DistanceCalculator.java
@@ -12,7 +12,14 @@ public class DistanceCalculator {
 
     public DistanceCalculator() {}
 
-
+    /**
+     * Calculates the distance between 2 cells form the chosen map
+     * @param map The 2D matrix, where this does the searching/calculations
+     * @param start The cell, where the calculation (and search) starts
+     * @param destination The cell, where the calculation (and search) ends
+     * @return The number of steps, that needs to be taken to reach the destination cell from the starting cell,
+     * if the two cells are not connected by RoadCell, this value is -1
+     */
     public static int distance(TiledMapTileLayer map, CityCell start, CityCell destination) {
         if (start == null || destination == null){return -1;}
 
@@ -24,7 +31,7 @@ public class DistanceCalculator {
 
         distances[start.getX()][start.getY()] = 0;
 
-        //Queue always has Citycells "sorted" by their distances in increasing order (It's standard FIFO queue.)
+        //Queue always has CityCells "sorted" by their distances in increasing order (It's standard FIFO queue.)
         LinkedList<CityCell> queue = new LinkedList<>();
         queue.addAll(getGoodNeighbours(map, start, distances));
 
@@ -38,9 +45,20 @@ public class DistanceCalculator {
         return distances[destination.getX()][destination.getY()];
     }
 
-
+    /**
+     * Searches for the closest workplace location from a LivingCellZone (based on certain conditions), meanwhile this
+     * counts the "steps" that this takes to reach that workplace CityCell <p>
+     * (More efficient and practical, than filtering all possible destination,
+     * and making a minimum-search with the distance method)
+     * @param map The 2D matrix, where this does the searching/calculations
+     * @param home the cell, where the search begins from
+     * @param workplaceType the class of the CityCell that this searches for
+     * @param mustBeAvailable If this true, this should check, if any ZoneCell, that this finds, is not full
+     * @return A tuple of the reached CityCell, that can qualify as a proper workplace (based on the conditions set in parameters),
+     * and the distance between the 2 CityCells, if there is no such workplace, this returns the tuple of (null, -1)
+     */
     public static Tuple<ZoneCell, Integer> closestOfWorkplaceWithDistance(TiledMapTileLayer map, LivingZoneCell home,
-                                                                   Class<?> workplaceType, boolean mustBeAvailable) {
+                                                                          Class<?> workplaceType, boolean mustBeAvailable) {
 
         //Setting up the distances matrix with -1 values, this will represent the unreachable tiles
         int[][] distances = new int[map.getWidth()][map.getHeight()];
@@ -74,7 +92,13 @@ public class DistanceCalculator {
         return (new Tuple<>(destination, distance));
     }
 
-
+    /**
+     * Collects the adjacent CityCells and calculates the distances between these CityCells and the starting cell of distances matrix
+     * @param map The 2D matrix, where this does the searching/calculations
+     * @param me The CityCell, which serves as the origin of this search
+     * @param distances The matrix, that contains which cell is how far from the starting point
+     * @return A list of proper CityCells, that are adjacent to this
+     */
     private static List<CityCell> getGoodNeighbours(TiledMapTileLayer map, CityCell me, int[][] distances){
         ArrayList<CityCell> result = new ArrayList<>();
 
@@ -114,7 +138,13 @@ public class DistanceCalculator {
         return result;
     }
 
-   
+    /**
+     * Checks if the coordinates are on the chosen map
+     * @param map The 2D matrix, where this does the searching/calculations
+     * @param x The distance of this from the origin on the horizontal axis
+     * @param y  The distance of this from the origin on the vertical axis
+     * @return True, if the given coordinates are on the chosen map
+     */
     public static boolean isReachable(TiledMapTileLayer map, int x, int y) {
         return (x >= 0 && x < map.getWidth() && y >= 0 && y < map.getHeight());
     }
