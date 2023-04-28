990d1c496ecab5ae16abdd1c892724d5df8ab212
Marci Jakab <x5mc0i@inf.elte.hu>
2023-04-28 16:56:52 +0200

    added road building logic to some cells. 2x2 cells now crash the game

--- core/src/hu/nem3d/zincity/Cell/BuildingCell.java
+++ core/src/hu/nem3d/zincity/Cell/BuildingCell.java
@@ -18,7 +18,7 @@ public abstract class BuildingCell extends CityCell {
         NorthWest, NorthEast, SouthWest, SouthEast
     }
     protected String name;  //Need for stats --Newton
-    boolean isSimple;
+
     protected BuildingPart part;
     protected int range;
     //protected int maintenanceFee; removed in favour of universal "upkeepCost" variable.
@@ -31,7 +31,7 @@ public abstract class BuildingCell extends CityCell {
 
     protected BuildingCell(int x, int y, TiledMapTileLayer tileLayer) throws CellException {
         super(x, y, tileLayer);
-        isSimple = true; //not sure why we need this
+
         this.isWired = true;
         if (!hasRoadNeighbor()){
             throw new CellException("Can't build that there");
@@ -48,7 +48,7 @@ public abstract class BuildingCell extends CityCell {
      */
     protected BuildingCell(int x, int y, TiledMapTileLayer tileLayer, BuildingPart part) throws CellException{
         super(x, y, tileLayer);
-        isSimple = false; //not sure why we need this
+
         this.isWired = true;
         this.part = part;
         if (!hasRoadNeighbor()){
@@ -65,44 +65,19 @@ public abstract class BuildingCell extends CityCell {
     }
 
 
-    public void setPart(BuildingPart part_){
-        if(!isSimple){
-            this.part = part_;
-        }
 
-    }
     public BuildingPart getPart() {
         return part;
     }
 
     private boolean hasRoadNeighbor(){ //i know it is stupid, but builder class takes care of the rest.
-        if (isSimple){
+
             return (this.getNeighbor(Direction.NORTH) != null && this.getNeighbor(Direction.NORTH).getClass() == RoadCell.class ||
                     this.getNeighbor(Direction.SOUTH) != null && this.getNeighbor(Direction.SOUTH).getClass() == RoadCell.class ||
                     this.getNeighbor(Direction.EAST) != null && this.getNeighbor(Direction.EAST).getClass() == RoadCell.class ||
                     this.getNeighbor(Direction.WEST) != null && this.getNeighbor(Direction.WEST).getClass() == RoadCell.class);
-        }
-        else if (part == BuildingPart.NorthWest){ //only checking from the north west part now, because thats where it's "built from"
-            CityCell northEastPartCell = this.getNeighbor(Direction.EAST);
-            CityCell southWestPartCell = this.getNeighbor(Direction.SOUTH);
-            CityCell southEastPartCell = this.getNeighbor(Direction.SOUTH).getNeighbor(Direction.EAST);
 
-            return (this.getNeighbor(Direction.NORTH) != null && this.getNeighbor(Direction.NORTH).getClass() == RoadCell.class ||
-                    this.getNeighbor(Direction.SOUTH) != null && this.getNeighbor(Direction.SOUTH).getClass() == RoadCell.class ||
 
-                    northEastPartCell.getNeighbor(Direction.NORTH) != null && this.getNeighbor(Direction.NORTH).getClass() == RoadCell.class ||
-                    northEastPartCell.getNeighbor(Direction.EAST) != null && this.getNeighbor(Direction.EAST).getClass() == RoadCell.class ||
-
-                    southWestPartCell.getNeighbor(Direction.WEST) != null && this.getNeighbor(Direction.WEST).getClass() == RoadCell.class ||
-                    southWestPartCell.getNeighbor(Direction.SOUTH) != null && this.getNeighbor(Direction.SOUTH).getClass() == RoadCell.class ||
-
-                    southEastPartCell.getNeighbor(Direction.EAST) != null && this.getNeighbor(Direction.EAST).getClass() == RoadCell.class ||
-                    southEastPartCell.getNeighbor(Direction.SOUTH) != null && this.getNeighbor(Direction.SOUTH).getClass() == RoadCell.class);
-
-        }
-        else{
-            return true;
-        }
     }
 }
 
