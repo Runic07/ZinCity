558db0faf75e669c1f76e3f717e5d4d3819a1c11
Bertóti Bálint <bcv6vb@inf.elte.hu>
2023-05-03 17:19:14 +0200

    Adding cell tests

---
+++ core/src/hu/nem3d/zincity/Test/CityCellTest.java
@@ -0,0 +1,45 @@
+package hu.nem3d.zincity.Test;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import hu.nem3d.zincity.Cell.*;
+import hu.nem3d.zincity.Misc.CellException;
+import hu.nem3d.zincity.Misc.Direction;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CityCellTest {
+
+    @Test
+    public void testIsWired(){
+        try {
+            TiledMapTileLayer map = new TiledMapTileLayer(2,2,1,1);
+            map.setCell(0, 1, new EmptyCell(0, 1, map));
+            map.setCell(0, 0, new RoadCell(0, 0, map));
+            FireStationCell cell = new FireStationCell(0, 1, map);
+            map.setCell(0, 1, cell);
+            assertTrue(cell.isWired());
+        } catch (CellException ce) {
+            fail("Failed Initialization: " + ce.getMessage());
+        }
+    }
+
+    @Test
+    public void testNeigbor(){
+        TiledMapTileLayer map = new TiledMapTileLayer(2,2,1,1);
+        CityCell cell = new EmptyCell(0, 1, map);
+        map.setCell(0, 1, cell);
+        map.setCell(0, 0, new RoadCell(0, 0, map));
+        assertEquals(RoadCell.class, cell.getNeighbor(Direction.SOUTH).getClass());
+    }
+
+    @Test
+    public void testRadius(){
+        TiledMapTileLayer map = new TiledMapTileLayer(2,2,1,1);
+        CityCell cell1 = new EmptyCell(0, 1, map);
+        map.setCell(0, 1, cell1);
+        CityCell cell2 = new RoadCell(0, 0, map);
+        map.setCell(0, 0, cell2);
+        assertEquals(1.0, cell1.auraRadiusSize(cell2), 1);
+    }
+}
