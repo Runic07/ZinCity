8e040fc635c671f3c7f1be1c44b3c92cc19c627a
Bertóti Bálint <bcv6vb@inf.elte.hu>
2023-05-06 22:14:16 +0200

    Updating, adding tests
    This commit would belong to the JUnit issue

--- core/src/test/CityTest.java
+++ core/src/test/CityTest.java
@@ -1,10 +1,8 @@
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import hu.nem3d.zincity.Cell.IndustrialZoneCell;
-import hu.nem3d.zincity.Cell.LivingZoneCell;
-import hu.nem3d.zincity.Cell.RoadCell;
-import hu.nem3d.zincity.Cell.ServiceZoneCell;
+import hu.nem3d.zincity.Cell.*;
 import hu.nem3d.zincity.Logic.Citizen;
 import hu.nem3d.zincity.Logic.City;
+import hu.nem3d.zincity.Logic.CityMap;
 import hu.nem3d.zincity.Misc.CellException;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -12,7 +10,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CityTest {
-    @Ignore
     @Test
     public void testAddingCitizen(){
         try {
@@ -24,12 +21,17 @@ public class CityTest {
             map.setCell(0, 2, new IndustrialZoneCell(0, 2, map));
 
             assertTrue(city.addCitizen());
-        } catch(CellException ce) {
-            fail("Failed Initialization: " + ce.getMessage());
+        } catch(Exception e) {
+            fail("Failed Initialization: " + e.getMessage());
         }
     }
 
-    @Ignore
+    @Test
+    public void testAddingCitizenFail(){
+        City city = new City();
+        assertFalse(city.addCitizen());
+    }
+
     @Test
     public void testSatisfactionDecrease(){
         try {
@@ -50,12 +52,11 @@ public class CityTest {
             }else{
                 fail("Error occurred while adding citizen!");
             }
-        } catch(CellException ce) {
-            fail("Failed Initialization: " + ce.getMessage());
+        } catch(Exception e) {
+            fail("Failed Initialization: " + e.getMessage());
         }
     }
 
-    @Ignore
     @Test
     public void testSatisfactionIncrease(){
         try {
@@ -78,8 +79,70 @@ public class CityTest {
             }else{
                 fail("Error occurred while adding citizen!");
             }
-        } catch(CellException ce) {
-            fail("Failed Initialization: " + ce.getMessage());
+        } catch(Exception e) {
+            fail("Failed Initialization: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testUpkeepCost(){
+        try {
+            City city = new City();
+            double lastBudget = city.budget;
+
+            TiledMapTileLayer map = city.getCityMap().getBuildingLayer();
+            map.setCell(0, 0, new RoadCell(0, 0, map));
+            map.setCell(0, 1, new PoliceCell(0, 1, map));
+
+            city.step();
+
+            assertTrue(lastBudget > city.budget);
+        } catch(Exception e) {
+            fail("Failed Initialization: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testTax(){
+        try {
+            City city = new City();
+            double lastBudget = city.budget;
+
+            TiledMapTileLayer map = city.getCityMap().getBuildingLayer();
+            map.setCell(0, 0, new RoadCell(0, 0, map));
+            map.setCell(1, 0, new LivingZoneCell(1, 0, map));
+            map.setCell(0, 1, new IndustrialZoneCell(0, 1, map));
+            city.addCitizen();
+
+            city.taxCoefficient = 2.0;
+
+            city.step();
+            city.step();
+
+            assertTrue(lastBudget < city.budget);
+        } catch(Exception e) {
+            fail("Failed Initialization: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCityMapCounts(){
+
+        try {
+            CityMap cm = new CityMap();
+            TiledMapTileLayer layer = cm.getBuildingLayer();
+
+            layer.setCell(0, 0, new RoadCell(0, 0, layer));
+            layer.setCell(1, 0, new ServiceZoneCell(1, 0, layer));
+            layer.setCell(0, 1, new IndustrialZoneCell(0, 1, layer));
+
+            layer.setCell(2, 2, new RoadCell(2, 2, layer));
+            layer.setCell(3, 2, new ServiceZoneCell(3, 2, layer));
+            layer.setCell(2, 3, new IndustrialZoneCell(2, 3, layer));
+
+            assertEquals(0, cm.ServiceZoneCount() - cm.IndustrialZoneCount());
+        } catch(Exception e) {
+            fail("Failed Initialization: " + e.getMessage());
         }
     }
 }
