1ee9831137eecb5ca63a2e8cf51227543bf70813
Bertóti Bálint <bcv6vb@inf.elte.hu>
2023-05-06 22:11:55 +0200

    Adding Generator effect

--- core/src/hu/nem3d/zincity/Cell/GeneratorCell.java
+++ core/src/hu/nem3d/zincity/Cell/GeneratorCell.java
@@ -3,6 +3,10 @@ package hu.nem3d.zincity.Cell;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import hu.nem3d.zincity.Misc.BuildingEffect;
 import hu.nem3d.zincity.Misc.CellException;
+import hu.nem3d.zincity.Misc.Direction;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class GeneratorCell extends BuildingCell {
 
@@ -12,10 +16,35 @@ public class GeneratorCell extends BuildingCell {
         this.price = 20;
         this.range = 0;
         this.upkeepCost = 7.5;
+
+        spreadEffect();
     }
 
     @Override
     public BuildingEffect getMyEffect() {
         return null;
+    }
+
+    @Override
+    protected void spreadEffect() {
+        boolean[][] visited = new boolean[tileLayer.getWidth()][tileLayer.getHeight()];
+
+        visited[x][y] = true;
+        LinkedList<CityCell> queue = new LinkedList<>();
+        queue.add(this);
+
+        while(!queue.isEmpty()) {
+            CityCell current = queue.remove(0);
+            if(!current.isElectrified() && current.setElectrified(true)) {
+                //System.out.println("Electrified: " + current.getX() + " " + current.getY());
+                Arrays.stream(Direction.values())
+                        .map(current::getNeighbor)
+                        .filter((neighbor) -> (neighbor != null && !visited[neighbor.getX()][neighbor.getY()]))
+                        .forEach((cell) -> {
+                            visited[cell.getX()][cell.getY()] = true;
+                            queue.add(cell);
+                        });
+            }
+        }
     }
 }
